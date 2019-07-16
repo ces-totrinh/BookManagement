613be78aee5ddac9b231556f586914a20d60852b
tototrinh <to.trinh@codeenginestudio.com>
2019-07-16 12:14:58 +0700

    BM_DAO: Create function to check the existence of Book

--- src/net/bookmanagement/dao/BookDAO.java
+++ src/net/bookmanagement/dao/BookDAO.java
@@ -90,6 +90,41 @@ public class BookDAO {
 		return book;
 		
 	}
+	@SuppressWarnings("unchecked")
+	public Boolean checkBookExisted(String bookName, String categoryName, List<String> authorNames ) {
+		Transaction transaction = null;
+		Boolean checkExisted = false;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try{
+			transaction = session.beginTransaction();
+			
+			String conditionOfAuthor = "";
+			for(String n: authorNames) {
+				conditionOfAuthor += "and author_name LIKE '%" + n + "%'";
+			}
+			
+			System.out.print(conditionOfAuthor.substring(3));
+			List<Book> book = session.createQuery(""
+					+ "from Book b "
+					+ "JOIN FETCH b._authors a "
+					+ "JOIN FETCH b._category c "
+					+ "where book_name=:bookName "
+					+ "and category_name=:categoryName "
+					+ "and (" + conditionOfAuthor.substring(3) +")").setParameter("bookName", bookName).setParameter("categoryName", categoryName).getResultList();
+			
+			if (book.size() > 0) checkExisted = true;
+			transaction.commit();
+		
+		} catch(Exception e) {
+			if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+		}
+		session.close();
+		return checkExisted;
+		
+	}
 	
 	@SuppressWarnings("unchecked")
 	public List<Book> getAllBooks(){
