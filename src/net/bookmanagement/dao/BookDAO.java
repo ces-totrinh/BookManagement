6ace15ad1c797f2d14f90b53b4fc3c4dc8fa55ea
tototrinh <to.trinh@codeenginestudio.com>
2019-07-17 14:17:04 +0700

    BM_DAO: Error correction returns null with the list and format code

--- src/net/bookmanagement/dao/BookDAO.java
+++ src/net/bookmanagement/dao/BookDAO.java
@@ -1,5 +1,6 @@
 package net.bookmanagement.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -10,143 +11,130 @@ import net.bookmanagement.utils.HibernateUtil;
 
 public class BookDAO {
 	public void saveBook(Book book) {
-        Transaction transaction = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            // start a transaction
-            transaction = session.beginTransaction();
-            // save the book object
-            session.saveOrUpdate(book);
-            // commit transaction
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-        session.close();
-    }
-	
+		Transaction transaction = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			transaction = session.beginTransaction();
+			session.saveOrUpdate(book);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		session.close();
+	}
+
 	public void updateBook(Book book) {
-        Transaction transaction = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            // start a transaction
-            transaction = session.beginTransaction();
-            // save the book object
-            session.update(book);
-            // commit transaction
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-        session.close();
-    }
-	
+		Transaction transaction = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			transaction = session.beginTransaction();
+			session.update(book);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		session.close();
+	}
+
 	public void deleteBookById(int id) {
 		Transaction transaction = null;
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		try {
-			// start a transaction
 			transaction = session.beginTransaction();
-			//delete a book object
 			Book book = session.get(Book.class, id);
 			if (book != null) {
 				session.delete(book);
 			}
 			transaction.commit();
 		} catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
 		session.close();
 	}
-	
+
 	public Book getBookById(int id) {
 		Transaction transaction = null;
 		Book book = null;
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		try{
-			//Start a transaction
+		try {
 			transaction = session.beginTransaction();
-			// get Book object by id
 			book = session.get(Book.class, id);
-			//commit transaction
 			transaction.commit();
-		
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
+				transaction.rollback();
+			}
+			e.printStackTrace();
 		}
 		session.close();
 		return book;
-		
+
 	}
+
 	@SuppressWarnings("unchecked")
-	public Boolean checkBookExisted(String bookName, String categoryName, List<String> authorNames ) {
+	public Boolean checkBookExisted(String bookName, String categoryName, List<String> authorNames) {
 		Transaction transaction = null;
 		Boolean checkExisted = false;
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		try{
+		try {
 			transaction = session.beginTransaction();
-			
+
 			String conditionOfAuthor = "";
-			for(String n: authorNames) {
+			for (String n : authorNames) {
 				conditionOfAuthor += "and author_name LIKE '%" + n + "%'";
 			}
-			
+
 			System.out.print(conditionOfAuthor.substring(3));
-			List<Book> book = session.createQuery(""
-					+ "from Book b "
-					+ "JOIN FETCH b._authors a "
-					+ "JOIN FETCH b._category c "
-					+ "where book_name=:bookName "
-					+ "and category_name=:categoryName "
-					+ "and (" + conditionOfAuthor.substring(3) +")").setParameter("bookName", bookName).setParameter("categoryName", categoryName).getResultList();
-			
-			if (book.size() > 0) checkExisted = true;
+			List<Book> book = session
+					.createQuery("" + "from Book b " + "JOIN FETCH b._authors a " + "JOIN FETCH b._category c "
+							+ "where book_name=:bookName " + "and category_name=:categoryName " + "and ("
+							+ conditionOfAuthor.substring(3) + ")")
+					.setParameter("bookName", bookName).setParameter("categoryName", categoryName).getResultList();
+
+			if (book.size() > 0)
+				checkExisted = true;
 			transaction.commit();
-		
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
+				transaction.rollback();
+			}
+			e.printStackTrace();
 		}
 		session.close();
 		return checkExisted;
-		
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<Book> getAllBooks(){
-		Transaction transaction = null;
-		 List <Book> listOfBook = null;
-		 Session session = HibernateUtil.getSessionFactory().openSession();
-		 try {
-			 transaction = session.beginTransaction();
-			 
-			 listOfBook = session.createQuery("from Book")
-			            .getResultList();	 
-			 transaction.commit();
-			 
-		 } catch ( Exception e) {
-			 if ( transaction != null) {
-				 transaction.rollback();
-			 }
-			 e.printStackTrace();
-		 }
-		 session.close();
-		 return listOfBook;
-	}
-	
-}
 
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Book> getAllBooks() {
+		Transaction transaction = null;
+		List<Book> listOfBook = new ArrayList<>();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			transaction = session.beginTransaction();
+
+			listOfBook = session.createQuery("from Book").getResultList();
+			transaction.commit();
+
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		session.close();
+		return listOfBook;
+	}
+
+}
