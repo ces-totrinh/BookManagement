e736dfbb8cc5799ef4eae3740497fb816e7ae122
tototrinh <to.trinh@codeenginestudio.com>
2019-07-17 14:20:34 +0700

    BM_UTILS: update the order of the modifications

--- src/net/bookmanagement/utils/HibernateUtil.java
+++ src/net/bookmanagement/utils/HibernateUtil.java
@@ -8,32 +8,32 @@ import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 
+	public static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+
+		getSessionFactory().close();
+	}
+
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
 	private static SessionFactory buildSessionFactory() {
 		try {
 
-			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-					.configure("hibernate.cfg.xml").build();
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml")
+					.build();
 
 			Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
 
 			return metadata.getSessionFactoryBuilder().build();
-		}
-		catch (Throwable ex) {
+		} catch (Throwable ex) {
 
 			System.err.println("Initial SessionFactory creation failed." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
 	}
 
-	public static SessionFactory getSessionFactory() {
-
-		return sessionFactory;
-	}
-
-	public static void shutdown(){
-
-		getSessionFactory().close();
-	}
 }
